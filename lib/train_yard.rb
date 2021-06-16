8868bcbc31ae4175713b9888b637626cbb3a20d1
Carina Sweet <carina.sweet06@gmail.com>
2021-06-16 10:57:11 -0600

    Update overflow cars method

--- lib/train_yard.rb
+++ lib/train_yard.rb
@@ -40,13 +40,20 @@ class TrainYard
     inventory
   end
 
+  def cargo_car_array
+    @trains.flat_map do |train|
+      train.cargo.flat_map do |car, count|
+        car
+      end
+    end
+  end
+
 #if car is on more than one train and inventory is > 10
   def overflow_cars
     overflow = []
-    require "pry"; binding.pry
-    @trains.each do |train|
-      train.cargo.each do |car, count|
-        if total_inventory[car] > 10
+    @trains.select do |train|
+      train.cargo.select do |car, count|
+        if total_inventory[car] > 10 && cargo_car_array.count(car) > 1
           overflow << car
         end
       end
